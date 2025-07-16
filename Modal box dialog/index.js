4cf09c441374ffee40454f7577816a5451c88bbf
Joshuaesc <escarezjohnjoshuamanalo@gmail.com>
2025-07-16 22:55:56 +0800

    Update index.js

--- Modal box dialog/index.js
+++ Modal box dialog/index.js
@@ -18,3 +18,32 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+//Method kapag yung button ay nasa loob ng loop sa madaling sabi madami at dumadami pa
+
+
+
+    var modal = document.getElementById("Modal");
+    var modalBackground = document.getElementById("ModalBg");
+    var buttons = document.querySelectorAll(".ButtonRegister");
+    var Close = document.getElementsByClassName("Close")[0];
+
+
+
+       buttons.forEach(function(button){
+           button.onclick = function(){
+                modal.style.display = "flex";
+                modalBackground.style.display = "flex";
+           };
+       });
+    
+
+      Close.onclick = function(){
+        modal.style.display = "none";
+        modalBackground.style.display = "none";
+    }
+
+
+
+
